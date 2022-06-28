be865f8b7440a39767d43153d390afa3f7a0bef8
Ranenburger Pro <usethchance@gmail.com>
2022-06-28 23:02:31 +0300

    херня

--- Traffic Lights/ViewController.swift
+++ Traffic Lights/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redLight: UIImageView!
+    @IBOutlet var yellowLight: UIImageView!
+    @IBOutlet var greenLight: UIImageView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = redLight.frame.width / 2
+        greenLight.layer.cornerRadius = redLight.frame.width / 2
     }
 
-
+    @IBAction func changeLights(_ sender: UIButton) {
+    }
+    
 }
 
